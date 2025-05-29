313b147488fc87772aefe2fa6380fe8caffdc2d4
GabrielSyR20 <gabriel.benja.syr@gmail.com>
2025-05-29 00:12:03 -0300

    que hace cada validacion

--- Validar.py
+++ Validar.py
@@ -1,4 +1,4 @@
-def es_entero(valor_int):
+def es_entero(valor_int): # Verifica si el valor es un entero
     if type(valor_int) == int:
         return True
     
@@ -10,7 +10,7 @@ def es_entero(valor_int):
     
     return False
 
-def notas_validas(notas):
+def notas_validas(notas): # Verifica si las notas están en el rango válido
     if notas >= 1 and notas <= 10:
         return True
     else:
